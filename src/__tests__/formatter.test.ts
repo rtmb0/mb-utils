3d9be7270737428a0c26ab200ab5e7ef8c45b2b5
Roberto Trapani <r.trapani@miaburton.com>
2022-08-08 10:14:10 +0200

    setup npm package

---
+++ src/__tests__/formatter.test.ts
@@ -0,0 +1,170 @@
+import { SizeFormatter } from '../formatter/sizeFormatter';
+import { Size } from '../formatter/size';
+
+describe('formatSizeWithSeparator', () => {
+    it('should ignore empty string returns empty string', () => {
+        const size = Size.create('')
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizeWithSeparator()
+        expect(formattedSize).toBe('')
+    })
+})
+
+describe('formatSizeWithSeparator', () => {
+    it('should not be format double digit string size and return the original value', () => {
+        const size = Size.create('46')
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizeWithSeparator()
+        expect(formattedSize).toBe('46')
+    })
+})
+
+describe('formatSizeWithSeparator', () => {
+    it('should not format double digit number size but should be converted to string', () => {
+        const size = Size.create(46)
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizeWithSeparator()
+        expect(formattedSize).toBe('46')
+    })
+})
+
+describe('formatSizeWithSeparator', () => {
+    it('should format four digit string size with a separator', () => {
+        const size = Size.create('4622')
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizeWithSeparator()
+        expect(formattedSize).toBe('46-22')
+    })
+})
+
+describe('formatSizeWithSeparator', () => {
+    it('should format four digit number size with a separator', () => {
+        const size = Size.create(4622)
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizeWithSeparator()
+        expect(formattedSize).toBe('46-22')
+    })
+})
+
+// format to HTML
+
+describe('formatSizesToHTML', () => {
+    it('should format both number convertible strings and pure numbers within mixed typed array', () => {
+        const size = Size.create(['4422', 4624, 4822])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>44-22 (mm)</strong> <br> <strong>46-24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should ignore non formattable sizes and format both number convertible strings and pure numbers within mixed typed array', () => {
+        const size = Size.create(['4422', 4624, 4822, 'notANumber'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>44-22 (mm)</strong> <br> <strong>46-24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should format only number convertible strings within an array of strings', () => {
+        const size = Size.create(['4422', '4624', '4822', 'abc', 'definitelyNotANumber'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>44-22 (mm)</strong> <br> <strong>46-24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should format both double and four digit convertible strings within an array of strings', () => {
+        const size = Size.create(['4422', '4624', '48'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>44-22 (mm)</strong> <br> <strong>46-24 (mm)</strong> <br> <strong>48 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should group bridge sizes, with a "/" separator, if they have the same eye value', () => {
+        const size = Size.create(['4622', '4624', '4626'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24/26 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should group bridge sizes ONLY with the same eye value', () => {
+        const size = Size.create(['4622', '4624', '4626', '4822'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24/26 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should format float', () => {
+        const size = Size.create(['4624', '4626', '46.22'])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-24/26 (mm)</strong> <br> <strong>46.22 (mm)</strong>`)
+    })
+})
+
+// falsy and strange mix of values
+
+describe('formatSizesToHTML', () => {
+    it('should ignore falsy types and return empty string', () => {
+        const size = Size.create([null, NaN, undefined, '', false])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe('')
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should ignore empty arrays and objects and return empty string', () => {
+        const size = Size.create([[], {}])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe('')
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should ignore falsy types and only return formatted truthy values', () => {
+        const size = Size.create([null, 4822, NaN, '4622', undefined, 4624])
+        const formatter = new SizeFormatter(size)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+// extra size tests
+
+describe('formatSizesToHTML', () => {
+    it('should properly format using extra size', () => {
+        const size = Size.create(['4822', '4622', '4624'])
+        const formatter = new SizeFormatter(size, '131')
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24 - 131 (mm)</strong> <br> <strong>48-22 - 131 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should ignore falsy extra size', () => {
+        const size = Size.create(['4822', '4622', '4624'])
+        const formatter = new SizeFormatter(size, '')
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
+
+describe('formatSizesToHTML', () => {
+    it('should ignore undefined extra size', () => {
+        const size = Size.create(['4822', '4622', '4624'])
+        const formatter = new SizeFormatter(size, undefined)
+        const formattedSize = formatter.formatSizesToHTML()
+        expect(formattedSize).toBe(`<strong>46-22/24 (mm)</strong> <br> <strong>48-22 (mm)</strong>`)
+    })
+})
---
+++ src/formatter/sizeFormatter.ts
@@ -0,0 +1,82 @@
+import { Size } from './size'
+
+export class SizeFormatter {
+  private groupedSizes: { [index: string]: string[] };
+  private sizeValue: string | string[];
+  private isArray: boolean;
+  private extraSizes: string;
+  private size: Size;
+
+  constructor(size: Size, extraSizes?: string) {
+    this.size = size;
+    this.sizeValue = size.value;
+    this.isArray = size.getIsArray;
+    this.extraSizes = extraSizes ? ` - ${extraSizes} ` : " ";
+    this.groupedSizes = {};
+    this.groupSizes();
+  }
+
+  private groupSizes() {
+    if (!this.isArray) return;
+
+    const groupedSizes: { [index: string]: string[] } = {};
+
+    for (const size of this.sizeValue) {
+      const sizeToNum = parseFloat(size);
+      const isSizeNumber = !isNaN(sizeToNum);
+
+      if (!isSizeNumber) continue;
+
+      const shouldBeFormatted = sizeToNum > 1000;
+
+      if (!shouldBeFormatted) {
+        groupedSizes[size] = groupedSizes.hasOwnProperty(size)
+          ? groupedSizes[size]
+          : [];
+        continue;
+      }
+
+      const eyeSize = sizeToNum.toString().substring(0, 2);
+      const otherSize = sizeToNum.toString().substring(2, 4);
+
+      groupedSizes[eyeSize] = groupedSizes.hasOwnProperty(eyeSize)
+        ? [...groupedSizes[eyeSize], otherSize]
+        : [otherSize];
+    }
+
+    const hasGroupedSizes = Object.keys(groupedSizes).length > 0;
+
+    this.groupedSizes = hasGroupedSizes ? groupedSizes : {};
+  }
+
+  formatSizesToHTML(separator: string = "/") {
+    if (!this.isArray) return "";
+
+    const formattedSizes = Object.keys(this.groupedSizes).reduce(
+      (acc: string[], key: string) => {
+        const hasMultipleSizes = this.groupedSizes[key].length > 0;
+        return hasMultipleSizes
+          ? [
+              ...acc,
+              `<strong>${key}-${this.groupedSizes[key].join(separator)}${
+                this.extraSizes
+              }(mm)</strong>`,
+            ]
+          : [...acc, `<strong>${key}${this.extraSizes}(mm)</strong>`];
+      },
+      []
+    );
+
+    return `${formattedSizes.join(" <br> ")}`;
+  }
+
+  formatSizeWithSeparator(separator: string = "-") {
+    if (this.sizeValue === "") return this.sizeValue;
+
+    const sizeToNum = parseFloat(this.sizeValue as string);
+
+    return sizeToNum > 1000
+      ? `${this.sizeValue.slice(0, 2)}${separator}${this.sizeValue.slice(2, 4)}`
+      : this.sizeValue;
+  }
+}
