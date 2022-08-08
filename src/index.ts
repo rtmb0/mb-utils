0481b480e60c2b1ffb85125d5856d42609f0bd52
Roberto Trapani <r.trapani@miaburton.com>
2022-08-08 10:20:59 +0200

    change export

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import {  SizeFormatter } from './formatter/sizeFormatter';
 import { Size } from './formatter/size';
 
-function formatWithSeparator (sizeVal: string|string[], separator?: string) {
+export function formatWithSeparator (sizeVal: string|string[], separator?: string) {
     const size = Size.create(sizeVal)
     const formatter = new SizeFormatter(size)
     return formatter.formatSizeWithSeparator(separator)
 }
 
-function formatToHTML (sizeVal: string|string[], extraSize?: string, separator?: string) {
+export function formatToHTML (sizeVal: string|string[], extraSize?: string, separator?: string) {
     const size = Size.create(sizeVal)
     const formatter = new SizeFormatter(size, extraSize)
     return formatter.formatSizesToHTML(separator)
 }
-
-export { formatWithSeparator, formatToHTML }
