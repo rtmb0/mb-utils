8f631aa7417f58b9fa64feac15f28db116f63a53
Roberto Trapani <r.trapani@miaburton.com>
2022-08-09 08:36:58 +0200

    fix exports while preserving commonjs module

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { SizeFormatter } from './formatter/sizeFormatter';
 import { Size } from './formatter/size';
 
-export function formatWithSeparator(sizeVal: string | string[], separator?: string) {
-  const size = Size.create(sizeVal);
+function formatSizeWithSeparator(sizeValue: string | string[], separator?: string) {
+  const size = Size.create(sizeValue);
   const formatter = new SizeFormatter(size);
   return formatter.formatSizeWithSeparator(separator);
 }
 
-export function formatToHTML(sizeVal: string | string[], extraSize?: string, separator?: string) {
-  const size = Size.create(sizeVal);
+function formatSizesToHTML(sizeValue: string | string[], extraSize?: string, separator?: string) {
+  const size = Size.create(sizeValue);
   const formatter = new SizeFormatter(size, extraSize);
   return formatter.formatSizesToHTML(separator);
 }
+
+export default { formatSizeWithSeparator, formatSizesToHTML };
