73e3ba5c536aff35db6b77a4210470be65062263
Roberto Trapani <r.trapani@miaburton.com>
2022-08-09 08:50:24 +0200

    named exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { SizeFormatter } from './formatter/sizeFormatter';
 import { Size } from './formatter/size';
 
-function formatSizeWithSeparator(sizeValue: string | string[], separator?: string) {
+export function formatSizeWithSeparator(sizeValue: string | string[], separator?: string) {
   const size = Size.create(sizeValue);
   const formatter = new SizeFormatter(size);
   return formatter.formatSizeWithSeparator(separator);
 }
 
-function formatSizesToHTML(sizeValue: string | string[], extraSize?: string, separator?: string) {
+export function formatSizesToHTML(sizeValue: string | string[], extraSize?: string, separator?: string) {
   const size = Size.create(sizeValue);
   const formatter = new SizeFormatter(size, extraSize);
   return formatter.formatSizesToHTML(separator);
 }
-
-export default { formatSizeWithSeparator, formatSizesToHTML };
